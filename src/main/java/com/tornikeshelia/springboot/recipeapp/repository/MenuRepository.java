cec04ab7030758d69ae54daa372e33991bc14979
Tornike <tornikeshelia1996@gmail.com>
2019-02-26 11:50:00 +0400

    Delete MenuRepository.java

--- src/main/java/com/tornikeshelia/springboot/recipeapp/repository/MenuRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tornikeshelia.springboot.recipeapp.repository;
-
-import com.tornikeshelia.springboot.recipeapp.entity.Menu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository extends JpaRepository<Menu,String> {
-}
