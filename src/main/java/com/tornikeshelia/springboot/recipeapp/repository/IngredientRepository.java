86a9b0dc9fdbcdc481c887e851df6479ebd9bc99
Tornike <tornikeshelia1996@gmail.com>
2019-02-26 11:49:49 +0400

    Delete IngredientRepository.java

--- src/main/java/com/tornikeshelia/springboot/recipeapp/repository/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tornikeshelia.springboot.recipeapp.repository;
-
-import com.tornikeshelia.springboot.recipeapp.entity.Ingredients;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientRepository extends JpaRepository<Ingredients,Integer> {
-}
